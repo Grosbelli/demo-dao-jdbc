2f938234cc68a719ace72df7aaa751e01789d454
Andrey Grosbelli <agrosbelli@yahoo.com.br>
2021-06-19 15:21:46 -0300

    department findAll implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -4,14 +4,22 @@ import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
+import java.util.List;
+
 public class Program2 {
     public static void main(String[] args) {
 
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
         System.out.println("=== TEST 1: department findById =====");
         Department department = departmentDao.findById(1);
         System.out.println(department);
 
+        System.out.println("=== TEST 2: department findAll =====");
+        List<Department> departments = departmentDao.findAll();
+        departments.forEach(x -> {
+            System.out.println(x);
+        });
 
     }
 }
